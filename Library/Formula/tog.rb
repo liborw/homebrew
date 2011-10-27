30e62c0039ee8502c5dc0a11a4bd5068fa0b8f4f
Libor Wagner <liborwagner@gmail.com>
2011-10-27 15:28:29 +0200

    Update: tog 0.4
    Md5 update

--- Library/Formula/tog.rb
+++ Library/Formula/tog.rb
@@ -1,19 +1,16 @@
 require 'formula'
 
 class Tog < Formula
-  url 'https://github.com/liborw/tog/tarball/0.3'
+  url 'https://github.com/liborw/tog/tarball/0.4'
   homepage 'https://github.com/liborw/tog'
-  head 'https://github.com/liborw/tog/tarball/develop'
-
-  if ARGV.include? '--HEAD'
-    md5 'cc4cd08e7131972c749e1936991f246d'
-  else
-    md5 'c2cdabe7d68e1dc25c36f4b02b8c7bfd'
-  end
+  md5 '131b36e889dba3a8beb43539cfd5b2c8'
 
   depends_on 'haskell-platform'
 
   def install
     system "cabal", "install", "--prefix=#{prefix}", "--user"
+
+    bash_completion_d = etc + 'bash_completion.d'
+    bash_completion_d.install 'tog-completion.bash'
   end
 end
